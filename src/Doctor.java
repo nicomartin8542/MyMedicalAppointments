56fa1c6464858f9a4f73a89ff2cdcb1f39ed2759
Nicolas <nicomartin854@gmail.com>
2022-04-25 23:38:49 -0300

    Segundo commit

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,12 +1,23 @@
 public class Doctor {
 
+    //Atributos
     int id;
     String name;
     String speciality;
 
+    //Constructor
+    Doctor(){
+        System.out.printf("Construyendo el objeto Doctor");
+    }
+
+    //Otro constructor
+    Doctor(String name) {
+        System.out.printf("El otro Dorctor es: " + name);
+    }
+
 
     //Comportamientos
     public void  showName (){
-        System.out.printf(name);
+        System.out.printf("\nEl nombre del Doctor es: " + name);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Martin");
         myDoctor.name = "Nicolas";
         myDoctor.showName();
     }
