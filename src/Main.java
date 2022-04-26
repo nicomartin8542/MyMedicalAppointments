4403104ac90021b4770be0a130dd7f3ca6fdcff4
Nicolas <nicomartin854@gmail.com>
2022-04-25 22:45:52 -0300

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Nicolas";
+        myDoctor.showName();
+    }
+}
+
+
+
